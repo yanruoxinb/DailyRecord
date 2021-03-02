61687bcaf0d000e3ac2e3fd933673a8f3922bd12
wangting <wangting.a@mininglamp.com>
2021-03-02 16:09:15 +0800

    修改本地文件

---
+++ MicroApp/unitTest/array_map.ts
@@ -0,0 +1,13 @@
+
+// const globalDataBase = makeGlobalDatabase();
+
+// function cleanDatabase(db) {
+//   db.cleanUp();
+// }
+// afterEach(() => {
+//   cleanUpDatabase(globalDataBase);
+// })
+
+describle('can find things', () => {
+  test('')
+})
---
+++ MicroApp/utils/array_map.ts
@@ -0,0 +1,39 @@
+/*
+ * 
+ *    ┏┓　　　┏┓
+ *  ┏┛┻━━━┛┻┓
+ *  ┃　　　　　　　┃
+ *  ┃　　　━　　　┃
+ *  ┃　＞　　　＜　┃
+ *  ┃　　　　　　　┃
+ *  ┃...　⌒　...　┃
+ *  ┃　　　　　　　┃
+ *  ┗━┓　　　┏━┛
+ *      ┃　　　┃　
+ *      ┃　　　┃
+ *      ┃　　　┃
+ *      ┃　　　┃  神兽保佑
+ *      ┃　　　┃  代码无bug　　
+ *      ┃　　　┃
+ *      ┃　　　┗━━━┓
+ *      ┃　　　　　　　┣┓
+ *      ┃　　　　　　　┏┛
+ *      ┗┓┓┏━┳┓┏┛
+ *        ┃┫┫　┃┫┫
+ *        ┗┻┛　┗┻┛
+ * 
+ * @Author: wangting
+ * @Date: 2021-02-09 16:15:08
+ * @LastEditTime: 2021-02-09 16:15:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /MicroApp/utils/array_map.ts
+ */
+
+function array_map(arr:Array<any>[]=[]) {
+   
+ }
+
+
+
+
